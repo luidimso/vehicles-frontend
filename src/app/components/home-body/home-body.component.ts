155a7ff32aaab5fff2584e4cfa213f72dceb350a
luidimso <luidi_mso@outlook.com>
2021-05-16 20:43:12 -0300

    Vehicle search function

--- src/app/components/home-body/home-body.component.ts
+++ src/app/components/home-body/home-body.component.ts
@@ -11,6 +11,7 @@ import { VehicleService } from 'src/app/services/vehicle.sevice';
 export class HomeBodyComponent implements OnInit, OnDestroy {
 
   vehicles:IVehicle[] = [];
+  vehiclesAux:IVehicle[] = [];
 
   private unsub:Subscription[] = [];
 
@@ -22,12 +23,29 @@ export class HomeBodyComponent implements OnInit, OnDestroy {
     this.unsub.push(
       this.vehicleServive.getVehicles().subscribe((response:IVehicle[]) => {
         this.vehicles = response;
+        this.vehiclesAux = this.vehicles;
       })
     );
   }
 
   ngOnDestroy(): void {
     this.unsub.forEach(u => u.unsubscribe());
+  }
+
+  searchVehicles(event:Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.vehicles = this.vehiclesAux;
+
+    if (filterValue && filterValue.trim() !== '') {
+      this.vehicles = this.vehicles.filter(function(fide) {
+        return (fide.chassi.toLowerCase().indexOf(filterValue.toLowerCase())>-1 ||
+          fide.marca.toLowerCase().indexOf(filterValue.toLowerCase())>-1 ||
+          fide.modelo.toLowerCase().indexOf(filterValue.toLowerCase())>-1 ||
+          fide.placa.toLowerCase().indexOf(filterValue.toLowerCase())>-1 ||
+          fide.renavam.toLowerCase().indexOf(filterValue.toLowerCase())>-1
+        );
+      });
+    }
   }
 
 }
