274b3458d7353f98db0b6b8917af60e5f6914c13
luidimso <luidi_mso@outlook.com>
2021-05-16 11:03:05 -0300

    Effect to shrink header when scrolling the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'vehicles-frontend';
+
+  smallHeader:boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {
+    window.onscroll = () => { this.checkScroll(); };
+  }
+
+  checkScroll() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      this.smallHeader = true;
+    } else {
+      this.smallHeader = false;
+    }
+  }
 }
